1c75223cb7a354981a9a63e17b817236c1ae872c
Peter <peter.polack@gmail.com>
2015-01-11 13:38:40 -0500

    trying again

--- plot4.R
+++ plot4.R
@@ -25,7 +25,7 @@ plot4 <- function() {
     plot(data$DateTime,data$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
     lines(data$DateTime,data$Sub_metering_2,col="red")
     lines(data$DateTime,data$Sub_metering_3,col="blue")
-    legend("topright", lty = 1, col = c("black","blue", "red"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),box.col="transparent",inset=.002)
+    legend("topright", lty = 1, col = c("black","blue", "red"), legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),box.col="transparent",inset=.009)
     plot(data$DateTime,data$Voltage,type="l",xlab="datetime",ylab="Voltage")
     plot(data$DateTime,data$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
     
